882e23f771fee196e052e2465e97e9c5e12f477f
Joshua <jdhawks@gmail.com>
2022-01-11 12:52:15 -0600

    update to users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::UsersController < ApplicationController
-	skip_before_action :authenticate_user, only: %i[create show]
+	skip_before_action :authenticate_user, only: %i[create]
 
 	def show
 		if current_user
--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,30 @@
-import { useCoursesQuery, useCourseQuery } from './store/guruAPI';
+import {
+	useCoursesQuery,
+	useCourseQuery,
+	useLoginMutation,
+	useCurrentUserQuery,
+	useLogoutMutation,
+} from './store/guruAPI';
+
+import { useEffect } from 'react';
 
 function App() {
 	const { data: courses } = useCoursesQuery();
 	const { data: course } = useCourseQuery(1);
+	const [login, { data: loginData }] = useLoginMutation();
+	const { data: currentUser } = useCurrentUserQuery();
 
-	console.log(courses, course);
+	useEffect(() => {
+		login({
+			username: 'josh',
+			password: '123',
+		});
+	}, []);
+
+	console.log(courses, course, loginData, currentUser);
+
 	return (
-		<div className='App'>
-			<h1>Page Count:{courses.length}</h1>
-		</div>
+		<div className='App'>{courses && <h1>Page Count:{courses.length}</h1>}</div>
 	);
 }
 
