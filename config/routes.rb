e36b6f774495fba89ab746569b9c31f921c472c1
Joshua <jdhawks@gmail.com>
2022-01-17 18:39:49 -0600

    fallback change

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
 		get '/me', to: 'users#show'
 		post '/login', to: 'sessions#create'
 		delete '/logout', to: 'sessions#destroy'
-		get '*path',
-		    to: 'fallback#index',
-		    constraints: ->(req) { !req.xhr? && req.format.html? }
 	end
+	get '*path',
+	    to: 'fallback#index',
+	    constraints: ->(req) { !req.xhr? && req.format.html? }
 end
