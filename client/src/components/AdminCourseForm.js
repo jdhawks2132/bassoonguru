90562450e1b8fbba1a1c3b668244b0745cf6574c
Joshua Hawks <jdhawks@gmail.com>
2022-01-28 09:51:43 -0600

    course and video form functionality complete

--- client/src/components/AdminCourseForm.js
+++ client/src/components/AdminCourseForm.js
@@ -1,42 +1,53 @@
 import React, { useState } from 'react';
+import { useAddCourseMutation } from '../store/guruAPI';
 
 function AdminCourseForm() {
 	const [name, setName] = useState('');
 	const [desc, setDesc] = useState('');
 	const [details, setDetails] = useState('');
 
+	const [addCourse, { error }] = useAddCourseMutation();
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		await addCourse({ name: name, desc: desc, details: details });
+	};
+
 	return (
-		<form className='auth-form-mini'>
-			<h2 className='page-header'>Add New Course:</h2>
-			<label>
-				<input
-					required
-					placeholder='name'
-					type='text'
-					onChange={(e) => setName(e.target.value)}
-					value={name}
-				/>
-			</label>
-			<label>
-				<input
-					required
-					placeholder='description'
-					type='text'
-					onChange={(e) => setDesc(e.target.value)}
-					value={desc}
-				/>
-			</label>
-			<label>
-				<input
-					required
-					placeholder='details'
-					type='text'
-					onChange={(e) => setDetails(e.target.value)}
-					value={details}
-				/>
-			</label>
-			<button className='btn-mini-blue'>Add</button>
-		</form>
+		<div>
+			<form onSubmit={handleSubmit} className='auth-form-mini'>
+				<h2 className='page-header'>Add New Course:</h2>
+				<label>
+					<input
+						required
+						placeholder='name'
+						type='text'
+						onChange={(e) => setName(e.target.value)}
+						value={name}
+					/>
+				</label>
+				<label>
+					<input
+						required
+						placeholder='description'
+						type='text'
+						onChange={(e) => setDesc(e.target.value)}
+						value={desc}
+					/>
+				</label>
+				<label>
+					<input
+						required
+						placeholder='details'
+						type='text'
+						onChange={(e) => setDetails(e.target.value)}
+						value={details}
+					/>
+				</label>
+				<button className='btn-mini-blue'>Add</button>
+			</form>
+			{error && <p className='error'>{JSON.stringify(error.data)}</p>}
+		</div>
 	);
 }
 
--- client/src/components/AdminVideoForm.js
+++ client/src/components/AdminVideoForm.js
@@ -1,42 +1,61 @@
 import React, { useState } from 'react';
+import Select from 'react-select';
+import { useCoursesQuery, useAddVideoMutation } from '../store/guruAPI';
 
 function AdminVideoForm() {
 	const [title, setTitle] = useState('');
 	const [url, setUrl] = useState('');
 	const [courseId, setCourseId] = useState('');
 
+	const { data: courses } = useCoursesQuery();
+
+	const [addVideo, { error }] = useAddVideoMutation();
+
+	const courseList = courses?.map((course) => ({
+		value: course.id,
+		label: course.name,
+	}));
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		await addVideo({ title: title, url: url, course_id: courseId });
+	};
+	console.log(courseId);
 	return (
-		<form className='auth-form-mini'>
-			<h2 className='page-header'>Add Video to Course:</h2>
-			<label>
-				<input
-					required
-					placeholder='title'
-					type='text'
-					onChange={(e) => setTitle(e.target.value)}
-					value={title}
-				/>
-			</label>
-			<label>
-				<input
-					required
-					placeholder='url'
-					type='text'
-					onChange={(e) => setUrl(e.target.value)}
-					value={url}
-				/>
-			</label>
-			<label>
-				<input
-					required
-					placeholder='course id'
-					type='number'
-					onChange={(e) => setCourseId(e.target.value)}
-					value={courseId}
-				/>
-			</label>
-			<button className='btn-mini-blue'>Add</button>
-		</form>
+		<div>
+			<form onSubmit={handleSubmit} className='auth-form-mini'>
+				<h2 className='page-header'>Add Video to Course:</h2>
+				<label>
+					<input
+						required
+						placeholder='title'
+						type='text'
+						onChange={(e) => setTitle(e.target.value)}
+						value={title}
+					/>
+				</label>
+				<label>
+					<input
+						required
+						placeholder='url'
+						type='text'
+						onChange={(e) => setUrl(e.target.value)}
+						value={url}
+					/>
+				</label>
+				<label>
+					<Select
+						className='select'
+						name='assignment_id'
+						onChange={(option) => setCourseId(option.value)}
+						value={courseId}
+						options={courseList}
+					/>
+				</label>
+				<button className='btn-mini-blue'>Add</button>
+			</form>
+			{error && <p className='error'>{JSON.stringify(error.data)}</p>}
+		</div>
 	);
 }
 
